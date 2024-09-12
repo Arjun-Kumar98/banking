b5e945d49464c0ee22bac68e954ccb629cfdc1d7
Arjun Kumar <39944807+Arjun-Kumar98@users.noreply.github.com>
2024-09-11 22:51:49 -0500

    Update new-loan.component.spec.ts

--- src/app/insertnewstuff/new-loan/new-loan.component.spec.ts
+++ src/app/insertnewstuff/new-loan/new-loan.component.spec.ts
@@ -33,7 +33,7 @@ describe('NewLoanComponent', () => {
     apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
       component.selectedBankId=1,
       component.selectedAccountId=1;
-      component.invoiceAmount=11203;
+      component.invoiceAmount=15204;
       component.invoiceDesc ="new Briefcase";
       component.selectedRetailerId =1;
     fixture.detectChanges();
